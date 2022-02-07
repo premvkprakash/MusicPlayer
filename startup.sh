db30ddb87ca8e2279aaae68f14c30ef28459466e
premvkprakash <79302210+premvkprakash@users.noreply.github.com>
2022-02-07 05:59:54 +0530

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,27 @@
+echo ">> FETCHING UPSTREAM..."
+git clone https://github.com/premvkprakash/MusicPlayer/ /MusicPlayer
+echo ">> INSTALLING REQUIREMENTS..."
+cd /MusicPlayer
+pip3 install -U -r requirements.txt
+echo ">> STARTING MUSIC PLAYER USERBOT..."
+clear
+echo "
+#     #                        
+##   ## #    #  ####  #  ####  
+# # # # #    # #      # #    # 
+#  #  # #    #  ####  # #      
+#     # #    #      # # #      
+#     # #    # #    # # #    # 
+#     #  ####   ####  #  ####  
+                               
+######                                    
+#     # #        ##   #   # ###### #####  
+#     # #       #  #   # #  #      #    # 
+######  #      #    #   #   #####  #    # 
+#       #      ######   #   #      #####  
+#       #      #    #   #   #      #   #  
+#       ###### #    #   #   ###### #    # 
+ @MIT_SUPPORT_BOT                                         
+MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
+"
+python3 main.py
